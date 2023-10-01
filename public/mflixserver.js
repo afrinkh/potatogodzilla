4c10837826a69cb645a7b5782020b1f4ce190ed1
arifmamon <arifmamon222@gmail.com>
2023-10-01 13:47:37 +0530

    Create mflixserver.js
    server

---
+++ public/mflixserver.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import HeaderRoute from './pages/header/headerRouter';
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+ReactDOM.render(
+    <HeaderRoute />,
+  document.getElementById('header')
+);
